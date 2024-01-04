f334098508f27478718c8cd24301af06764d97c5
minhtamDeveloper <minhtamceo2@gmail.com>
2024-01-04 14:50:19 +0700

    Commit UserService

---
+++ Project_2_Web_Api/Project_2_Web_Api/Service/Impl/UserServiceImpl.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using Project_2_Web_API.Models;
+
+
+namespace Project_2_Web_Api.Service.Impl;
+
+public class UserServiceImpl : UserService
+{
+	private DatabaseContext db;
+
+/*	public IActionResult Create(string UserJson)
+	{
+		try
+		{
+			
+			
+		}
+		catch(Exception ex)
+		{
+			
+		}
+	}*/
+
+	public Task<IActionResult> Delete(string id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IActionResult> FindAll()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IActionResult> Update(string UserJson)
+	{
+		throw new NotImplementedException();
+	}
+
+	Task<IActionResult> UserService.Create(string UserJson)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Project_2_Web_Api/Project_2_Web_Api/Service/UserService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_2_Web_Api.Service;
+
+public interface UserService
+{
+	Task<IActionResult> Create(string UserJson);
+	Task<IActionResult> Delete(string id);
+	Task<IActionResult> Update(string UserJson);
+	Task<IActionResult> FindAll();
+
+}
